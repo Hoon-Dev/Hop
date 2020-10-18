6d20170ceddc53a598877012b9c8e8ff0bcdcf6a
Kim Jae Hoon <hoon.development@gmail.com>
2020-10-19 08:31:48 +0900

    Add animation state

--- src/js/hop.js
+++ src/js/hop.js
@@ -5,22 +5,26 @@
     
     window.Hop = (function(){
         var hopCount = 0;
+
+        /* shadow object */
         var shadow = {
+            duration : ".5",
+            color : "rgba(0, 0, 0, 0.7)",
             isShadowed: false,
             open: function(){
                 if(hopCount === 0){
-                    hop_Collection.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-                    hop_Collection.style.transitionDuration = ".2s";
+                    hop_Collection.style.transitionDuration = this.duration + "s";
+                    hop_Collection.style.backgroundColor = this.color;
                     hop_Collection.style.zIndex = "999";
                 }
             },
             close: function(){
                 if(hopCount === 0){
+                    hop_Collection.style.transitionDuration = this.duration + "s";
                     hop_Collection.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
-                    hop_Collection.style.transitionDuration = ".2s";
-                    setTimeout(function(){
+                    hop_Collection.addEventListener("transitionend", function(){
                         hop_Collection.style.zIndex = "-999";
-                    }, 200);
+                    });
                 }
             }
         };
@@ -33,10 +37,12 @@
         }
         
         return {
-            add: function(param){
-                this[param.name] = new (function(name, body, events){
+            add: function({name, body, events}, effect){
+                /* popup object */
+                this[name] = new (function(){
                     var self = this;
                     var isOpened = false;
+                    effect = (effect || {animation: "", duration: ".2"});
 
                     this.open = function(time, callback){
                         if(isOpened === false){
@@ -54,14 +60,26 @@
                             div.setAttribute("id", name);
                             div.innerHTML = body;
                             if(events != undefined){
-                                for(var i=0;i<events.length;i++){
+                                for(var i=0; i<events.length; i++){
                                     eventMapping.bind(self)(div, i, events);
                                 }
                             }
 
                             hop_Collection.appendChild(div);
+                            div.style.left = "calc(50% - "+(div.offsetWidth/2)+"px)";
                             div.style.top = "calc(50% - "+(div.offsetHeight/2)+"px - 50px)";
-                            div.style.transitionDuration = "10s";
+                            div.style.transitionDuration = effect.duration + "s";
+
+                            switch(effect.animation){
+                                case "Jump":
+                                    div.style.animationName = "Jump-Open";
+                                    div.style.animationDuration = effect.duration + "s";
+                                    div.style.animationPlayState = "running";
+                                    break;
+                                default:
+                                    break;
+                            }
+
                             div.style.opacity = 1;
                             
                             hopCount += 1;
@@ -70,18 +88,22 @@
                     }
                     this.close = function(){
                         if(isOpened === true){
-                            hop_Collection.removeChild(document.getElementById(param.name));
+                            hop_Collection.removeChild(document.getElementById(name));
                             hopCount -= 1;
                             isOpened = false;
                             shadow.close();
                         }
                     }
-                })(param.name, param.body, param.events);
+                })();
             },
             remove: function(name){
                 if(name != undefined && this.hasOwnProperty(name)){
                     delete this[name];
                 }
+            },
+            setShadow: function(duration, color){
+                shadow.duration = (duration || ".2");
+                shadow.color = (color || "rgba(0, 0, 0, 0.7)");
             }
         }
     })();
